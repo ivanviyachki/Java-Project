bce14a01e7df540146e1e5a990f4b8ed2e7b1dff
Simeon Chakarov <simeonchakarov@Simeons-MacBook-Pro.local>
2021-01-17 17:23:20 +0200

    change home message

--- courseProject/courseproject-ui/src/views/Home.vue
+++ courseProject/courseproject-ui/src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <img alt="Logo" src="../assets/logo.png">
     <h3 style="color: aliceblue; font-family: 'Al Nile'; padding-top: 30px">Welcome to our demo!</h3>
-    <p style="color: azure">We are entrepreneurs ready to conquer the world. The first step we made is our innovative system for managing zoos</p>
+    <p style="color: azure">We are entrepreneurs ready to conquer the world. </p>
+    <p style="color: azure">The first step we made is our innovative system for a massive search engine of animals in different Zoo franchises</p>
   </div>
 </template>
 <style>
